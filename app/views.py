0be2195d903e27fa4aa9bada0b45d3c1fbc26024
Teddie <teddymachoka98@yahoo.com>
2020-09-09 12:34:52 +0300

    moved the instance folder outside the app folder

--- app/views.py
+++ app/views.py
@@ -8,8 +8,11 @@ def index():
     '''
     View root page function that returns the index page and its data
     '''
-    message = 'Hello World'
-    return render_template('index.html',message = message)
+    # Getting popular movie
+    popular_movies = get_movies('popular')
+    print(popular_movies)
+    title = 'Home - Welcome to The best Movie Review Website Online'
+    return render_template('index.html',title = title, popular = popular_movies)
 
 @app.route('/movie/<int:movie_id>')
 def movie(movie_id):
@@ -17,4 +20,5 @@ def movie(movie_id):
     '''
     View movie page function that returns the movie details page and its data
     '''
+    
     return render_template('movie.html',id = movie_id)
